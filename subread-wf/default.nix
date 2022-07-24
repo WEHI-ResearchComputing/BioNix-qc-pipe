2d56dee1f376295d90ffe5536052644ee009d82d
victorwkb <victorwkb@gmail.com>
2022-07-24 22:51:39 +1000

    fix code

--- subread-wf/default.nix
+++ subread-wf/default.nix
@@ -5,19 +5,19 @@
 }:
 
 with bionix;
-with lib
+with lib;
 
 let
   preprocess = flip pipe [
     (subread.align { inherit ref; })
-    (sambamba.sort { nameSort = true; })
+    (sambamba.sort { })
   ];
 
   inputs = [
     # Sample 1
       {
         input1 = fetchFastQ {
-          url = https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-1.fq";
+          url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-1.fq";
           sha256 = "0kh29i6fg14dn0fb1xj6pkpk6d83y72g7aphkbvjrhm82braqkm8";
         };
 
@@ -26,6 +26,7 @@ let
           sha256 = "0czk85km6a91y0fn4b7f9q7ps19b5jf7jzwbly4sgznps7ir2kdk";
         };
       }
+  ];
   ref = fetchFastA {
     url = "https://github.com/PapenfussLab/bionix/raw/master/examples/ref.fa";
     sha256 = "0sy9hq8n55knfkiblam50dzaiwhrx6pv8b8l1njdn6kfj4wflz2p";
