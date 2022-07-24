dc13580fca5cb1339b8949d23421ddba52919fa2
victorwkb <victorwkb@gmail.com>
2022-07-24 22:33:39 +1000

    fix flake and lock

--- subread-wf/flake.nix
+++ subread-wf/flake.nix
@@ -1,11 +1,22 @@
 {
-  description = "A flake for building sample workflow using subread";
+  description = "Flake for sample workflow using subread";
 
   inputs = {
     bionix.url = "github:papenfusslab/bionix";
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, ... }: {
-  };
+  outputs = { self, nixpkgs, bionix, flake-utils }: 
+
+    flake-utils.lib.eachDefaultSystem
+      (system: with bionix.lib
+        {
+          nixpkgs = import nixpkgs {
+            inherit system;
+          };
+        }; {
+      defaultPackage = callBionix ./. { };
+      packages.small = callBionix ./. { small = true; };
+      });
 }
