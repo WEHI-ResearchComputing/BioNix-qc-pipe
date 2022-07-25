78413c3c849df5de59bf6b930b0634efa28759ae
victorwkb <victorwkb@gmail.com>
2022-07-25 14:06:41 +1000

    fix hash and post result

--- subread-wf/default.nix
+++ subread-wf/default.nix
@@ -18,7 +18,7 @@ let
       {
         input1 = fetchFastQ {
           url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-1.fq";
-          sha256 = "0kh29i6fg14dn0fb1xj6pkpk6d83y72g7aphkbvjrhm82braqkm8";
+          sha256 = "qE6s8hKowiz3mvCq8/7xAzUz77xG9rAcsI2E50xMAk4=";
         };
 
         input2 = fetchFastQ {
