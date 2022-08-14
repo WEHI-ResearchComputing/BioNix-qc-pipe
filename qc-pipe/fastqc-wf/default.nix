e1cc368352398430c7ac4fa1e2305ad22b5fcf45
victorwkb <victorwkb@gmail.com>
2022-08-14 14:41:31 +1000

    fastqc-wf init

---
+++ qc-pipe/fastqc-wf/default.nix
@@ -0,0 +1,30 @@
+# Example workflow to run fastqc on sample for data qc
+
+{ bionix ? import <bionix> { }
+, ref ? bionix.ref.grch38.seq
+}:
+
+with bionix;
+with lib;
+
+let
+  inputs =
+    {
+      input1 = fetchFastQ {
+        url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-1.fq";
+        sha256 = "qE6s8hKowiz3mvCq8/7xAzUz77xG9rAcsI2E50xMAk4=";
+      };
+
+      input2 = fetchFastQ {
+        url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-2.fq";
+        sha256 = "0czk85km6a91y0fn4b7f9q7ps19b5jf7jzwbly4sgznps7ir2kdk";
+      };
+    };
+
+  ref = fetchFastA {
+    url = "https://github.com/PapenfussLab/bionix/raw/master/examples/ref.fa";
+    sha256 = "0sy9hq8n55knfkiblam50dzaiwhrx6pv8b8l1njdn6kfj4wflz2p";
+  };
+
+in 
+lib.mapAttrs (_: val: fastqc.check { input = val; }) inputs
