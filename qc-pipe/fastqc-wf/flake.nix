6b2da1edf7f17c6ff11b80d54d77abf6d7379238
victorwkb <victorwkb@gmail.com>
2022-08-14 22:37:15 +1000

    init

---
+++ qc-pipe/fastqc-wf/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for workflow on qc human genome";
+
+  inputs = {
+    bionix.url = "github:papenfusslab/bionix";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, bionix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem 
+      (system: with bionix.lib
+        {
+          nixpkgs = import nixpkgs { inherit system; };
+        };
+        {
+          defaultPackage = callBionix ./. { };
+        };
+      );
+}
