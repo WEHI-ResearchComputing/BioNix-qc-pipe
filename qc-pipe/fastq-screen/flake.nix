ed014641a77a82a53380823cf0d335e637626f3b
victorwkb <victorwkb@gmail.com>
2022-08-15 14:22:11 +1000

    flake init

---
+++ qc-pipe/fastq-screen/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake fastq-screen-wf on ecoli genome";
+
+  inputs = {
+    bionix.url = "github:papenfusslab/bionix";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, bionix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: with bionix.lib
+        {
+          nixpkgs = import nixpkgs { inherit system; };
+        };
+        {
+          defaultPackage = callBionix ./. { };
+        }
+      );
+}
